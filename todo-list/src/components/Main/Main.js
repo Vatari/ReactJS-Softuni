da112ddb687ee99b52c9e9ec81ed0720989e3d12
vatari <majestic13@gmail.com>
2023-03-01 11:32:45 +0200

    fetch to practice server

--- todo-list/src/components/Main/Main.js
+++ todo-list/src/components/Main/Main.js
@@ -1,94 +1,39 @@
+import { useEffect } from "react";
+
 import Spinner from "../Spinner/Spinner";
 const Main = () => {
+  const baseUrl = "http://localhost:3030/jsonstore/todos";
+
+  useEffect(() => {
+    fetch(baseUrl)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
-    <main class="main">
-      <section class="todo-list-container">
+    <main className="main">
+      <section className="todo-list-container">
         <h1>Todo List</h1>
 
-        <div class="add-btn-container">
-          <button class="btn">+ Add new Todo</button>
+        <div className="add-btn-container">
+          <button className="btn">+ Add new Todo</button>
         </div>
 
-        <div class="table-wrapper">
+        <div className="table-wrapper">
           <Spinner />
-          <table class="table">
+          <table className="table">
             <thead>
               <tr>
-                <th class="table-header-task">Task</th>
-                <th class="table-header-status">Status</th>
-                <th class="table-header-action">Action</th>
+                <th className="table-header-task">Task</th>
+                <th className="table-header-status">Status</th>
+                <th className="table-header-action">Action</th>
               </tr>
             </thead>
             <tbody>
-              <tr class="todo is-completed">
+              <tr className="todo is-completed">
                 <td>Give dog a bath</td>
                 <td>Complete</td>
-                <td class="todo-action">
-                  <button class="btn todo-btn">Change status</button>
-                </td>
-              </tr>
-
-              <tr class="todo is-completed">
-                <td>Do laundry</td>
-                <td>Complete</td>
-                <td class="todo-action">
-                  <button class="btn todo-btn">Change status</button>
-                </td>
-              </tr>
-
-              <tr class="todo">
-                <td>Vacuum floor</td>
-                <td>Incomplete</td>
-                <td class="todo-action">
-                  <button class="btn todo-btn">Change status</button>
-                </td>
-              </tr>
-
-              <tr class="todo is-completed">
-                <td>Feed cat</td>
-                <td>Complete</td>
-                <td class="todo-action">
-                  <button class="btn todo-btn">Change status</button>
-                </td>
-              </tr>
-
-              <tr class="todo">
-                <td>Change light bulbs</td>
-                <td>Incomplete</td>
-                <td class="todo-action">
-                  <button class="btn todo-btn">Change status</button>
-                </td>
-              </tr>
-
-              <tr class="todo is-completed">
-                <td>Feed cat</td>
-                <td>Complete</td>
-                <td class="todo-action">
-                  <button class="btn todo-btn">Change status</button>
-                </td>
-              </tr>
-
-              <tr class="todo">
-                <td>Change light bulbs</td>
-                <td>Incomplete</td>
-                <td class="todo-action">
-                  <button class="btn todo-btn">Change status</button>
-                </td>
-              </tr>
-
-              <tr class="todo is-completed">
-                <td>Go to Store</td>
-                <td>Completed</td>
-                <td class="todo-action">
-                  <button class="btn todo-btn">Change status</button>
-                </td>
-              </tr>
-
-              <tr class="todo">
-                <td>Fill gas tank</td>
-                <td>Incomplete</td>
-                <td class="todo-action">
-                  <button class="btn todo-btn">Change status</button>
+                <td className="todo-action">
+                  <button className="btn todo-btn">Change status</button>
                 </td>
               </tr>
             </tbody>
