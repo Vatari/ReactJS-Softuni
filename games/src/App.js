8d86437deb2d13bfcc938c700625afa4714a93cd
vatari <majestic13@gmail.com>
2023-03-16 10:56:54 +0200

    added home page

--- games/src/App.js
+++ games/src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div id="box">
       <Header />
+      <Home />
     </div>
   );
 }
