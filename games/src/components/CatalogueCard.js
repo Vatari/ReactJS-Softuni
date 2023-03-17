3bfdf7e4fb454e28a0a29f9daf594a451b0b47ca
vatari <majestic13@gmail.com>
2023-03-17 09:57:35 +0200

    added route to Details

--- games/src/components/CatalogueCard.js
+++ games/src/components/CatalogueCard.js
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 const CatalogueCard = (game) => {
   return (
     <div className="allGames">
       <div className="allGames-info">
-        <img src={game.imageUrl} />
+        <img src={game.imageUrl} alt="game" />
         <h6>Genre: {game.genre}</h6>
         <h2>{game.title}</h2>
-        <a href="#" className="details-button">
+        <Link to={`/catalogue/${game._id}`} className="details-button">
           Details
-        </a>
+        </Link>
       </div>
     </div>
   );
